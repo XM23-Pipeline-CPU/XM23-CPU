71175ef943d40658e033dd8b6919c04eaf180303
saar <saar.omessi@gmail.com>
2024-05-24 22:28:36 -0700

    ALU initial version - switch based

---
+++ FPGA/ALU/alu_BIC.sv
@@ -0,0 +1,8 @@
+// alu_BIC.sv
+module alu_BIC (
+    input logic [15:0] a,    // Operand A
+    input logic [15:0] b,    // Operand B
+    output logic [15:0] result // Result
+);
+    assign result = (a & ~(1 << b)); // Bitwise AND operation
+endmodule
