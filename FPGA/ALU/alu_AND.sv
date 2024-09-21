acea2492a8187a3ab0b43c98d39a1a1f5acfbadf
QuitePoshLlama <vlad.christian.chiriac@gmail.com>
2024-09-21 08:39:20 -0700

    deleting temp ALU project

--- FPGA/ALU/alu_AND.sv
+++
@@ -1,8 +0,0 @@
-// alu_AND.sv
-module alu_AND (
-    input logic [15:0] a,    // Operand A
-    input logic [15:0] b,    // Operand B
-    output logic [15:0] result // Result
-);
-    assign result = a & b; // Bitwise AND operation
-endmodule
--- FPGA/ALU/alu_XOR.sv
+++
@@ -1,8 +0,0 @@
-// alu_XOR.sv
-module alu_XOR (
-    input logic [15:0] a,    // Operand A
-    input logic [15:0] b,    // Operand B
-    output logic [15:0] result // Result
-);
-    assign result = a ^ b; // Bitwise AND operation
-endmodule
